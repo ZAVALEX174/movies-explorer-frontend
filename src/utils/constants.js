2ea18ce18f609627c53b8d2a17c8cc96a91e3491
Alexandr Zuev <zuevalex@yandex.ru>
2023-08-23 02:34:50 +0500

    fix card-movie and error donload, fix BUSE_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://api.zuevdiplom.nomoredomains.xyz';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.zuevdiplom.nomoredomains.xyz';
+// export const BASE_URL = 'http://localhost:3000';
 export const MOVIE_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 
 export const checkResponse = (res) => {
